a06b31c46f82e447b1fc33da85d9ae3bb0155077
SoselN <nikitasodel8@gmail.com>
2024-03-28 18:48:57 +0300

    ..

--- Project53/Source.cpp
+++ Project53/Source.cpp
@@ -1,9 +1,45 @@
 #include <iostream>
 #include <string>
-#include <Student.h>
+#include "Student.h"
 
 using namespace std;
 
 int main() {
+	//int n{10};
+	// int n = 10;
+	//int array[10]{}
+	// int array[10]{1, 2, 3, 4, 5}
+	// int array[4]{1, 2, 3, 4}
+	//for (int i = 0; i < 10; i++)
+	//{
+	//	cout << array[i];
+	//}
 
+	//cout << endl;
+
+	/*Student s1("Alex", 14, 9);
+	Student s2("Alice");
+	Student s3(10);
+	Student s4;
+	Student s5{};
+	Student s6{"Peter"};
+	Student s7{ "Anna", 13, 7 };*/
+	//Student s8{ "Anna", 13}; Error
+	//Student s9 = Student("Alice");
+
+	const int size = 3;
+	Student students[size];
+	//Student students[size]{ {"Alice"}, {10}, {"Peter", 14, 9} };
+
+	/*cout << s1.convert() << endl;
+	cout << s2.convert() << endl;
+	cout << s3.convert() << endl;
+	cout << s4.convert() << endl;
+	cout << s5.convert() << endl;
+	cout << s6.convert() << endl;
+	cout << s7.convert() << endl;*/
+	//cout << s8.convert() << endl;
+	//cout << s9.convert() << endl;
+
+	return 0;
 }
--- Project53/Student.h
+++ Project53/Student.h
@@ -1,36 +1,55 @@
-#pragma once
+п»ї#pragma once
 #include <string>
 #include <iostream>
 
 using namespace std;
 
 class Student {
-publick:
+public:
 	string name;
 	int age;
 	double mark;
 
 
-//default constructor (конструктор по умолчанию)
+//default constructor (РєРѕРЅСЃС‚СЂСѓРєС‚РѕСЂ РїРѕ СѓРјРѕР»С‡Р°РЅРёСЋ)
 	Student() {
+		cout << "default constructor" << endl;
 		name = "no name";
-		age = 5;
-		mark = 4;
+		age = 4;
+		mark = 5;
 }
 	//canonical constructor with parameters
 	Student(string nm, int a, double m) {
+		cout << "canonical constructor with parameters" << endl;
 		name = nm;
 		age = a;
 		mark = m;
 	}
 	//constructor with parameters
 	Student(string nm) {
+		cout << "constructor with parameters 1" << endl;
 		name = nm;
 		age = 0;
 		mark = 0;
 	}
+
+	Student(double m) {
+		cout << "constructor with parameters 2" << endl;
+		name = "no name";
+		age = 4;
+		mark = m;
+	}
+	// destructor ~
+	~Student() {
+		cout << "destructor" << endl;
+	}
+
 	string convert() {
 		string s = "";
+		s += name;
+		s += " age is " + to_string(age);
+		s += "and " + name + " mark is " + to_string(mark);
+		cout << s << endl;
 	}
 
 };
